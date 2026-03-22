9df61386b9977ff14574b95a085c7cb0ee07a375
vapvapvapvapvap <165909028+vapvapvapvapvap@users.noreply.github.com>
2026-03-22 17:40:33 +0500

    Add files via upload

---
+++ products.cpp
@@ -0,0 +1,12 @@
+#include "Products.h"
+#include <iostream>
+
+using namespace std;
+
+void showProducts() {
+    cout << "\nДоступные товары:\n";
+    cout << "1. Яблочный сок - 120\n";
+    cout << "2. Апельсиновый сок - 150\n";
+    cout << "3. Луковый сок - 80\n";
+    cout << "4. Петрушевый чай - 75\n";
+}
---
+++ products.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+struct Product {
+    std::string name;
+    double price;
+};
+
+void showProducts();
